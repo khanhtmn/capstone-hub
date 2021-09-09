ceff897141ebe8d07e7bc241d056857e085a4956
Khanh Nguyen <khanh@minerva.kgi.edu>
2021-09-08 22:23:35 -0400

    add cards

--- front-end/src/Header.js
+++ front-end/src/Header.js
@@ -15,28 +15,16 @@
 // export default Header;
 
 import MinervaLogo from './assets/MinervaLogo.svg';
-import Vector from './assets/Vector.svg';
+import UpsideDownTriangle from './assets/UpsideDownTriangle.svg';
+import MagnifyingGlass from './assets/MagnifyingGlass.svg';
 import Ava from './assets/Ava.svg';
 import './Header.css';
 
 function Header() {
   return (
     <div className="Header">
-      <div className="Vertical">
-        
-        <div className="Horizontal">
-          <img src={MinervaLogo} className="HeaderLogo" alt="Header logo" />
-          <div className="SearchBar"></div>
-          <div className="Toggle" style={{width:107}}>
-            <p>Major</p>
-            <img src={Vector} className="Vector"/>          
-          </div>
-          <div className="Toggle" style={{width:178}}>
-            <p>Project Feature</p>
-            <img src={Vector} className="Vector"/>          
-            </div>
-        </div>
-
+      <div className="LeftBarVertical">
+        <img src={MinervaLogo} className="HeaderLogo" alt="Header logo" />
         {/* <div className="Base-vertical"></div> */}
         <div className="AvaText">
           <img src={Ava} className="Avatar" alt="Avatar"/>
@@ -46,6 +34,39 @@ function Header() {
             </div>
         </div>
       </div>
+
+      <div className="CardBox">
+        <div className="TopNavBar">
+          <div className="SearchBar">
+            <img src={MagnifyingGlass} className="MagnifyingGlass"/>
+          </div>
+          <div className="Toggle" style={{width:107}}>
+            <p>Major</p>
+            <img src={UpsideDownTriangle} className="UpsideDownTriangle"/>          
+          </div>
+          <div className="Toggle" style={{width:178}}>
+            <p>Project Feature</p>
+            <img src={UpsideDownTriangle} className="UpsideDownTriangle"/>          
+          </div>
+        </div>
+
+        <div className="CardsCollection">
+          <div className="Card">
+            <p className="UserFirstname">Khanh Nguyen</p>
+            <p>Major: CS - Applied Problem Solving | Minor: SS</p>
+            <p>Project Features:</p>
+            <p>An interdisciplinary projects to leverage technology for social goods. My current idea is to build a platform for mentoring women and non-binary students in Computer Science. The idea may change over time as I do research more, but ...</p>
+          </div>
+          <div className="Card">
+            <p className="UserFirstname">Khanh Nguyen</p>
+            <p>Major: CS - Applied Problem Solving | Minor: SS</p>
+            <p>Project Features:</p>
+            <p>An interdisciplinary projects to leverage technology for social goods. My current idea is to build a platform for mentoring women and non-binary students in Computer Science. The idea may change over time as I do research more, but ...</p>
+          </div>
+        </div>
+
+      </div>
+
 
     </div>
   );
