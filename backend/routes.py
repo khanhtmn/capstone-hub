13161622ce5ac35eed35dcf88dea22cb3bb4af49
Khanh Nguyen <khanh@minerva.kgi.edu>
2021-10-30 13:16:16 -0400

    working main page UI

--- backend/routes.py
+++ backend/routes.py
@@ -1,4 +1,4 @@
-from flask import current_app, jsonify, make_response
+from flask import current_app, request, jsonify, make_response
 from app import create_app, db
 from models import Login, User, Project
 
@@ -16,35 +16,36 @@ def hello():
 
 @app.route("/projects", methods=["GET"], strict_slashes=False)
 def projects():
-    data = []
-    all_projects = Project.query\
-        .join(User, Project.user_id==User.id)\
-        .add_columns(User.firstname, User.lastname, User.primary_major,\
-            User.secondary_major, User.minor,\
-            Project.title, Project.abstract, Project.feature)\
-        .filter(Project.user_id==User.id)\
-        .all()
-    for project in all_projects:
-        data.append(
-            {
-                "firstname": project.firstname,
-                "lastname": project.lastname,
-                "primary_major": project.primary_major,
-                "secondary_major": project.secondary_major,
-                "minor": project.minor,
-                "title": project.title,
-                "abstract": project.abstract,
-                "feature": project.feature,
-            }
-        )
-    response = make_response(
-        jsonify(
-        data=data,
-        status=200
-        )
-    )
-    return response
+    if request.method == 'GET':
+        try:
+            data = []
+            all_projects = Project.query\
+                .join(User, Project.user_id==User.id)\
+                .add_columns(User.id, User.firstname, User.lastname,User.primary_major,\
+                    User.secondary_major, User.minor, User.primary_concentration,\
+                        Project.title, Project.abstract, Project.feature)\
+                .filter(Project.user_id==User.id)\
+                .all()
+            for project in all_projects:
+                data.append(
+                    {
+                        "id": project.id,
+                        "firstname": project.firstname,
+                        "lastname": project.lastname,
+                        "primary_major": project.primary_major,
+                        "secondary_major": project.secondary_major,
+                        "minor": project.minor,
+                        "title": project.title,
+                        "abstract": project.abstract,
+                        "feature": project.feature,
+                    }
+                )
+            response = make_response(jsonify(data=data, status=200))
 
+            return response
+
+        except Exception as e:
+            return(str(e))
 
 if __name__ == "__main__":
     app.run(debug=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,66 @@
 import { useState,useEffect } from 'react'
-import './App.css';
-import ArticleList from './Components/ArticleList'
+import MinervaLogo from './assets/MinervaLogo.svg';
+import UpsideDownTriangle from './assets/UpsideDownTriangle.svg';
+import MagnifyingGlass from './assets/MagnifyingGlass.svg';
+import SampleAvatar from './assets/SampleAvatar.svg';
+import './MainApp.css';
+import ProjectList from './Components/ProjectList'
 
-function App() {
-  const [articles, setArticles] = useState([]);
+function MainApp() {
+  const [projects, setProjects] = useState([]);
 
   // Modify the current state by setting the new data to
   // the response from the backend
   useEffect(()=>{
-    fetch('http://localhost:5000/articles',{
+    fetch('http://localhost:5000/projects',{
       'methods':'GET',
       headers : {
         'Content-Type':'application/json'
       }
     })
     .then(response => response.json())
-    .then(response => setArticles(response))
+    .then(response => setProjects(response))
     .catch(error => console.log(error))
 
   },[])
 
   return (
-    <div className="App container m-4">
-    <div className="row">
-      <div className="text-center">
-      <h1>Connecting a React Frontend to a Flask Backend.</h1>
+    <div className="MainApp m-4">
+
+      <div className="LeftNavBar">
+        <img src={MinervaLogo} className="HeaderLogo" alt="Header logo" />
+        <div className="AvaText">
+          <img src={SampleAvatar} className="Avatar" alt="Avatar"/>
+            <div className="PersonalInfo">
+              <p className="UserFirstname">Khanh</p>
+              <p className="UserClass">Class of 2022</p>
+            </div>
+        </div>
       </div>
-    </div>
 
-      <ArticleList 
-      articles={articles} 
-      />
+      <div className="MainBox">
+        <div className="TopNavBar">
+          <div className="SearchBar">
+            <img src={MagnifyingGlass} className="MagnifyingGlass"/>
+          </div>
+          <div className="Toggle" style={{width:107}}>
+            <p>Major</p>
+            <img src={UpsideDownTriangle} className="UpsideDownTriangle"/>          
+          </div>
+          <div className="Toggle" style={{width:178}}>
+            <p>Project Feature</p>
+            <img src={UpsideDownTriangle} className="UpsideDownTriangle"/>          
+          </div>
+        </div>
 
+        <ProjectList 
+        projects={projects.data} 
+        />
+
+      </div>
+  
     </div>
   );
 }
 
-export default App;
+export default MainApp;
