967aacc243be0e5259fae70cb9235e8541e12602
Khanh Nguyen <khanh@minerva.kgi.edu>
2022-02-25 17:37:16 -0800

    add features

--- frontend/src/Components/TopNavBar.js
+++ frontend/src/Components/TopNavBar.js
@@ -1,19 +1,16 @@
 import "./TopNavBar.css";
+import { useState, useEffect } from "react";
 
 import InputAdornment from "@mui/material/InputAdornment";
 import TextField from "@mui/material/TextField";
 import Search from "@mui/icons-material/Search";
-import { useState, useEffect } from "react";
-
 import OutlinedInput from "@mui/material/OutlinedInput";
 import MenuItem from "@mui/material/MenuItem";
 import ListItemText from "@mui/material/ListItemText";
 import Select from "@mui/material/Select";
 import Checkbox from "@mui/material/Checkbox";
-
 import InputLabel from "@mui/material/InputLabel";
 import FormControl from "@mui/material/FormControl";
-import { Button } from "@mui/material";
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -41,7 +38,6 @@ const TopNavBar = (props) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("Jello!");
     const filteredProjects = [];
     for (let project of props.projects) {
       for (let field in project) {
@@ -64,32 +60,46 @@ const TopNavBar = (props) => {
     props.setRenderedProjects(filteredProjects);
   };
   const handleChangeMajorFilter = (event) => {
-    console.log(event.target.value, "FROM HANDLE CHANGE");
+    // console.log(event.target.value, "FROM HANDLE CHANGE");
     setUserMajorFilters(
       typeof event.target.value === "string"
         ? event.target.value.split(",")
         : event.target.value
     );
   };
-  console.log(userMajorFilters, "from TopNavBar");
+  const handleChangeFeatureFilter = (event) => {
+    console.log(event.target.value, "FROM HANDLE CHANGE");
+    setUserFeatureFilters(
+      typeof event.target.value === "string"
+        ? event.target.value.split(",")
+        : event.target.value
+    );
+  };
+  // console.log(userMajorFilters, "from TopNavBar");
   return (
     <div className="TopNavBar">
-      <form onSubmit={handleSubmit}>
-        <TextField
-          id="input-with-icon-textfield"
-          value={searchValue}
-          onChange={(event) => setSearchValue(event.target.value)}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <Search />
-              </InputAdornment>
-            ),
-          }}
-          variant="standard"
-        />
-      </form>
-      <FormControl sx={{ m: 1, width: 120 }}>
+      <div className="SearchBox">
+        <form onSubmit={handleSubmit}>
+          <TextField
+            fullWidth
+            label="Search for project, name, etc."
+            id="fullWidth"
+            id="input-with-icon-textfield"
+            value={searchValue}
+            onChange={(event) => setSearchValue(event.target.value)}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Search />
+                </InputAdornment>
+              ),
+            }}
+            variant="standard"
+            color="success"
+          />
+        </form>
+      </div>
+      <FormControl sx={{ m: 1, width: 250 }}>
         <InputLabel id="demo-multiple-checkbox-label">Major</InputLabel>
         <Select
           labelId="demo-multiple-checkbox-label"
@@ -106,6 +116,34 @@ const TopNavBar = (props) => {
             <MenuItem key={majorFilter} value={majorFilter}>
               <Checkbox checked={userMajorFilters.indexOf(majorFilter) > -1} />
               <ListItemText primary={majorFilter} />
+            </MenuItem>
+          ))}
+          <div>
+            <button onClick={handleSubmit}>Apply</button>
+          </div>
+        </Select>
+      </FormControl>
+      <FormControl sx={{ m: 1, width: 120 }}>
+        <InputLabel id="demo-multiple-checkbox-label">
+          Project Feature
+        </InputLabel>
+        <Select
+          labelId="demo-multiple-checkbox-label"
+          id="demo-multiple-checkbox"
+          multiple
+          value={userFeatureFilters}
+          autoWidth
+          onChange={handleChangeFeatureFilter}
+          input={<OutlinedInput label="Project Feature" />}
+          renderValue={(selected) => selected.join(", ")}
+          MenuProps={MenuProps}
+        >
+          {props.featureFilters.map((featureFilter) => (
+            <MenuItem key={featureFilter} value={featureFilter}>
+              <Checkbox
+                checked={userFeatureFilters.indexOf(featureFilter) > -1}
+              />
+              <ListItemText primary={featureFilter} />
             </MenuItem>
           ))}
           <div>
